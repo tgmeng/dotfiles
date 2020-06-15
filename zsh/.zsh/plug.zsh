727a6178fafa1559d2f9f6f23a64dadc5accd0db
tgmeng <tgmeng@163.com>
2020-06-15 21:12:59 +0800

    refactor(zsh): 添加 oh-my-zsh docker 相关 plugins

--- zsh/.zsh/plug.zsh
+++ zsh/.zsh/plug.zsh
@@ -6,6 +6,8 @@ zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/history", from:oh-my-zsh
 zplug "plugins/autojump", from:oh-my-zsh
 zplug "plugins/emoji", from:oh-my-zsh
+zplug "plugins/docker", from:oh-my-zsh
+zplug "plugins/docker-compose", from:oh-my-zsh
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
